4b9d556daaa77b16310d9a7cf6dd476f510af77e
Arjun <arjun.pushpavannan@gmail.com>
2025-07-27 10:16:51 +1000

    remove

--- src/main/java/com/app/categorise/ContentApplication.java
+++ src/main/java/com/app/categorise/ContentApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application-secrets.properties")
 public class ContentApplication {
 
 	public static void main(String[] args) {

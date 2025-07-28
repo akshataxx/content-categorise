cfa54d39973ee32f143e9c266586880248f6532e
Akshata <akshata@hubsystems.com.au>
2025-07-29 09:51:10 +1000

    new dto for refresh tokens

---
+++ src/main/java/com/app/categorise/api/dto/auth/RefreshTokenRequest.java
@@ -0,0 +1,13 @@
+package com.app.categorise.api.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenRequest {
+    private String refreshToken;
+}

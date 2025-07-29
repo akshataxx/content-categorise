774ad2a5cc71cdc2313d6bc68d6f9f23c5d4b54e
Akshata <akshata@hubsystems.com.au>
2025-07-29 10:14:29 +1000

    added lombok

--- src/main/java/com/app/categorise/api/dto/auth/JwtAuthResponse.java
+++ src/main/java/com/app/categorise/api/dto/auth/JwtAuthResponse.java
@@ -1,26 +1,14 @@
 package com.app.categorise.api.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtAuthResponse {
     private String accessToken;
+    private String refreshToken;
     private String tokenType = "Bearer";
-
-    public JwtAuthResponse(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public String getTokenType() {
-        return tokenType;
-    }
-
-    public void setTokenType(String tokenType) {
-        this.tokenType = tokenType;
-    }
 } 

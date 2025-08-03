4b2c28902ca29756c90d97535ac11ba2832c23da
Akshata <akshata.dhuraji@gmail.com>
2025-08-03 15:39:25 +1000

    ensures db operation happens within a transaction

--- src/main/java/com/app/categorise/domain/service/RefreshTokenService.java
+++ src/main/java/com/app/categorise/domain/service/RefreshTokenService.java
@@ -1,11 +1,15 @@
 package com.app.categorise.domain.service;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import java.time.Instant;
 import java.util.UUID;
 
 interface RefreshTokenService {
+    @Transactional
     void save(UUID userId, String refreshToken, Instant expiry);
     boolean isValid(String refreshToken);
 
+    @Transactional
     void deleteByUserId(UUID userId);
 }
--- src/main/java/com/app/categorise/domain/service/RefreshTokenServiceImpl.java
+++ src/main/java/com/app/categorise/domain/service/RefreshTokenServiceImpl.java
@@ -3,6 +3,7 @@ package com.app.categorise.domain.service;
 import com.app.categorise.data.entity.RefreshTokenEntity;
 import com.app.categorise.data.repository.RefreshTokenRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -16,6 +17,7 @@ public class RefreshTokenServiceImpl implements RefreshTokenService {
     }
 
     @Override
+    @Transactional
     public void save(UUID userId, String refreshToken, Instant expiry) {
         repo.deleteByUserId(userId);
         repo.save(new RefreshTokenEntity(null, userId, refreshToken, expiry));
@@ -29,6 +31,7 @@ public class RefreshTokenServiceImpl implements RefreshTokenService {
     }
 
     @Override
+    @Transactional
     public void deleteByUserId(UUID userId) {
         repo.deleteByUserId(userId);
     }

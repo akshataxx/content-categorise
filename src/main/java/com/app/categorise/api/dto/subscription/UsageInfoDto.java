58bcf25136133c9339b36575a268fe6807869897
Akshata <akshata.dhuraji@gmail.com>
2025-09-30 08:42:28 +1000

    added logging and premium json property

--- src/main/java/com/app/categorise/api/dto/subscription/UsageInfoDto.java
+++ src/main/java/com/app/categorise/api/dto/subscription/UsageInfoDto.java
@@ -1,10 +1,15 @@
 package com.app.categorise.api.dto.subscription;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * DTO for user usage information
  */
 public class UsageInfoDto {
+    @JsonProperty("isPremium")
     private boolean isPremium;
+    
+    @JsonProperty("remainingFreeTranscriptions")
     private int remainingFreeTranscriptions;
     
     public UsageInfoDto() {}
@@ -14,9 +19,11 @@ public class UsageInfoDto {
         this.remainingFreeTranscriptions = remainingFreeTranscriptions;
     }
     
+    @JsonProperty("isPremium")
     public boolean isPremium() { return isPremium; }
     public void setPremium(boolean premium) { isPremium = premium; }
     
+    @JsonProperty("remainingFreeTranscriptions")
     public int getRemainingFreeTranscriptions() { return remainingFreeTranscriptions; }
     public void setRemainingFreeTranscriptions(int remainingFreeTranscriptions) { 
         this.remainingFreeTranscriptions = remainingFreeTranscriptions; 
